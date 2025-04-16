4f534d70554d1c8eeace6de205635e28114a9270
Your Name <you@example.com>
2025-04-16 18:47:18 +0200

    Auth

--- script.js
+++ script.js
@@ -1,9 +1,65 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGI9jrUNOimGa5rTub0NCG9uURJhX1cJE",
+  authDomain: "chatworld-42a91.firebaseapp.com",
+  databaseURL: "https://chatworld-42a91-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "chatworld-42a91",
+  storageBucket: "chatworld-42a91.firebasestorage.app",
+  messagingSenderId: "592866834390",
+  appId: "1:592866834390:web:fe20ce5caf8a13b1611677",
+  measurementId: "G-S12WG5QJ9T"
+};
+
+const app= initializeApp(firebaseConfig)
+
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged  } 
+from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+
 let gomb=document.getElementById("send")
 const message= document.getElementById("message")
 const messages = document.getElementById("messages")
 
+const googleBtn = document.getElementById("google")
+const signOutBtn = document.getElementById("signout")
+
 const api="https://chatworld-42a91-default-rtdb.europe-west1.firebasedatabase.app/messages.json"
 
+const auth= getAuth()
+const gProvider= new GoogleAuthProvider()
+
+let loggedUser={}
+
+googleBtn.onclick=signInWithGoogle
+signOutBtn.onclick = logOut
+
+function signInWithGoogle(){
+  signInWithPopup(auth, gProvider)
+}
+
+function logOut(){
+  signOut(auth)
+}
+
+onAuthStateChanged(auth, user =>{
+  // console.log("User: ", user.email)
+ loggedUser = {...user}
+  // console.log("User: ", loggedUser)
+  if (!user){
+    signOutBtn.classList.add("d-none")
+
+    googleBtn.classList.remove("d-none")
+    googleBtn.classList.add("d-block")
+
+  }
+  else{
+    signOutBtn.classList.remove("d-none")
+    signOutBtn.classList.add("d-block")
+
+    googleBtn.classList.remove("d-block")
+    googleBtn.classList.add("d-none")
+  } 
+})
 
 console.log("Gomb:", gomb)
 
@@ -14,7 +70,7 @@ gomb.addEventListener(
 async function sendMessage(){
   console.log("Value",message.value)
   const body={
-    user:"Attila",
+    user:loggedUser.email,
     message: message.value,
     date: Date.now()
   }
